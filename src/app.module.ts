a3843f6f5ad50f6f9c3de7166ccf580e1fc379ad
Neliia Zelienova <neliia@vormats.com>
2024-03-17 18:22:08 +0100

    First commit. Added DB schema, added auth guard with JWT and base services

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { APP_GUARD } from '@nestjs/core';
+import { RoomsController } from './controllers/rooms/rooms.controller';
+import { AuthGuard } from './guards/auth/auth.guard';
+import { PrismaService } from './services/prisma/prisma.service';
+import { UsersService } from './services/users/users.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [RoomsController],
+  providers: [PrismaService, UsersService, {
+    provide: APP_GUARD,
+    useClass: AuthGuard,
+  },],
 })
 export class AppModule {}
